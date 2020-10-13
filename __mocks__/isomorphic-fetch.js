e8e680882247d69787f2fbeff4e0a7fbbb139468
ferrjer <ferrjer@mfcgd.com>
2020-10-13 11:31:32 +0800

    Mock the fetch function

--- __mocks__/isomorphic-fetch.js
+++ __mocks__/isomorphic-fetch.js
@@ -1,4 +1,4 @@
 let __value = 42;
-const isomorphicFetch = jest.fn(()=>__value);
+const isomorphicFetch = jest.fn(()=>__value); // this is a spy
 isomorphicFetch.__setValue = v => __value = v;
 export default isomorphicFetch;
