eee47d9e541b67ee9cbe35afa9e8b5c3508509fe
ferrjer <ferrjer@mfcgd.com>
2020-10-14 09:23:12 +0800

    test component: QuestionDetail

--- src/components/__test__/QuestionDetail.test.js
+++ src/components/__test__/QuestionDetail.test.js
@@ -1,6 +1,41 @@
-describe('The QuestionDetail component', () => {
+import { mapStateToProps, QuestionDetailDisplay } from '../QuestionDetail';
+import renderer from 'react-test-renderer';
+import React from 'react';
 
-  it('should not regress', () => {
+describe(`The Question Detail component`, () => {
+  describe(`The Container Element`,()=>{
+    describe(`mapStateToProps`, ()=>{
+      it(`should map the state to props correctly`,()=>{
+        const sampleQuestion = {
+          question_id:42,
+          body:'Space is big'
+        };
+        const appState = {
+            questions:[sampleQuestion]
+        };
+        const ownProps = {
+            question_id:42,
+        };
+        const componentState = mapStateToProps(appState,ownProps);
+        // output: { question_id: 42, body: 'Space is big' }
+        console.log(componentState);
+        expect(componentState).toEqual(sampleQuestion);
+      });     
+    })
   });
-  
+
+  describe(`The display element`,()=>{
+    it(`should not regress`,()=>{
+      const tree = renderer.create(
+        <QuestionDetailDisplay
+         title="Thanks!"
+         body="43"
+         answer_count={0}
+         tags={[`hitchhiking`]}
+        />
+      );
+
+      expect(tree.toJSON).toMatchSnapshot();
+    });
+  });
 })
